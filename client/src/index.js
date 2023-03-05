784c17e97be3a6cdfca5cfbb5dee8505fd7158cd
paualarcon7 <alarconayelenpaulina@gmail.com>
2023-03-05 01:13:08 -0300

    configuring auth0

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,7 @@ import { Provider } from "react-redux";
 import { store } from "./store";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
+
 import axios from "axios";
 
 
@@ -14,14 +15,17 @@ axios.defaults.headers.common['Authorization'] = sessionStorage.getItem('user');
 
 axios.defaults.baseURL = 'https://streetartwineclub-backend-production.up.railway.app';
 
+const domain = "dev-6ttpzvp7k3ijg0l6.us.auth0.com"
+const clientId = "aAnP8ywOwAijCGOi8OsIKdidjFMdoeHl"
+
 ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
       <Auth0Provider
-        domain="dev-6ttpzvp7k3ijg0l6.us.auth0.com"
-        clientId="aAnP8ywOwAijCGOi8OsIKdidjFMdoeHl"
+           domain={domain}
+           clientId={clientId}
         authorizationParams={{
-          redirect_uri: "https://pf-street-art-wine-club-front-end.vercel.app/home",
+          redirect_uri: "https://street-art-wine-club-front-nnyerip46-paualarcon7.vercel.app/",
         }}
       >
         <App />
