e4d41ebc1942ef9e10b56f92236823fed6156a20
paualarcon7 <alarconayelenpaulina@gmail.com>
2023-03-05 01:53:51 -0300

    .

--- client/src/index.js
+++ client/src/index.js
@@ -25,7 +25,7 @@ ReactDOM.render(
            domain={domain}
            clientId={clientId}
         authorizationParams={{
-          redirect_uri: "https://street-art-wine-club-front-end-wc6e.vercel.app/",
+          redirect_uri: "https://street-art-wine-club-front-end-wc6e-paualarcon7.vercel.app/home",
         }}
       >
         <App />
